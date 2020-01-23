7d4efe620b0d5f8725654fd33460807e8df6cf42
Oscar Ore <oscar.ore2011@gmail.com>
2020-01-23 04:41:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,3 @@
 #lib/introduction.rb 
-Failures:
- 
-  1) #introduction takes in an argument of a name and puts out a phrase with that name using string interpolation
-     Failure/Error: expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
-     NoMethodError:
-       undefined method `introduction' for #<RSpec::ExampleGroups::Introduction:0x007fdbc499a938>
-     # ./spec/introduction_spec.rb:5:in `block (3 levels) in <top (required)>'
-     # ./spec/introduction_spec.rb:5:in `block (2 levels) in <top (required)>'
+def introduction(name)
+end
