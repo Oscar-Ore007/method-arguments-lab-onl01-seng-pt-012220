0a367f647364a85324645901d560100b0d1f65ff
Oscar Ore <oscar.ore2011@gmail.com>
2020-01-23 04:49:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-#lib/introduction_with_language.rb
+
 def introduction_with_language(name, language)
-  puts "Hi, my name is Dan and I am learning to program in Ember.js"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
